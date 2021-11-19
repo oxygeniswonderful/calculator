82994912df1632af700048727f1d7896d4845dfe
oxygeniswonderful <75098063+oxygeniswonderful@users.noreply.github.com>
2021-11-19 18:29:40 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,13 +1,5 @@
 public class Calculator {
-    public static void main(String[] args) {
-
-        int x = 5;
-        int y = 10;
-
-        Calculator mainClass = new Calculator();
-
-    }
-
+    
     public int plus(int number1, int number2) {
         return number1 + number2;
     }
